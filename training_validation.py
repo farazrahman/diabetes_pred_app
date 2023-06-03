062e00950463b841da7e269c29d5437fa808c7a9
farazrahman <faraz.rahman41@gmail.com>
2023-06-03 10:33:38 -0700

    trained ML and validated results

--- training_validation.py
+++ training_validation.py
@@ -1,10 +1,15 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, confusion_matrix, precision_recall_curve, roc_curve
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
 
 
 # clean the data before preprocessing
@@ -65,10 +70,56 @@ def augment_data(df: pd.DataFrame):
     return resampled_df
 
 
+def _evaluation_metrics(actuals, prediction, model, X_val):
+    # Evaluate the classifier
+    print(classification_report(actuals, prediction))
+
+    # plot confusion matrix
+    cm = confusion_matrix(actuals, prediction, labels=model.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+    disp.plot()
+    plt.title('Confusion matrix of validation data on base model')
+    plt.show()
+
+    # plot the feature importances
+    fe = model.feature_importances_.argsort()
+    plt.barh(X_val.columns[fe], model.feature_importances_[fe])
+    plt.xlabel("Feature Importance")
+    plt.title('Feature Importance chart')
+    plt.show()
+
+
+def _predict_probabilities(model, X_val, y_val):
+
+    #calculate the probabilities of getting diabetes instead of just 0 or 1
+    probabilities = model.predict_proba(X_val).tolist()
+
+    # add probabilities column in the test data set to check the predictions & probabilities side by side
+    new_val = X_val
+    new_val = new_val.reset_index()
+    prob_df = pd.DataFrame({'probabilities': probabilities})
+    prob_df.reset_index(inplace=True)
+    label_vals = y_val.to_frame()
+    label_vals.reset_index(inplace=True)
+    lst = [new_val, label_vals, prob_df]
+    validated_df = pd.concat(lst, axis=1)
+    validated_df.drop(columns=['index'], inplace=True)
+    print(validated_df)
+
+
 def train_model(resampled_df: pd.DataFrame):
+    """
+    This function takes the augmented dataframe, gets a random sample without replacement and trains a
+    classification ML model and outputs the pickled model and model metrics.
+    :param resampled_df: The augmented dataframe
+    :return: pickled model, model metrics and graphs
+    """
+    # sample 1% data
+    sampled_df = resampled_df.sample(frac=0.02, random_state=42)
+    print(sampled_df.info())
     # Split the balanced dataset into training and testing sets
-    X = resampled_df.drop('diabetes', axis=1)
-    y = resampled_df['diabetes']
+    X = sampled_df.drop('diabetes', axis=1)
+    y = sampled_df['diabetes']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Create a random forest classifier
@@ -80,8 +131,11 @@ def train_model(resampled_df: pd.DataFrame):
     # Make predictions on the test set
     y_pred = rf_classifier.predict(X_test)
 
-    # Evaluate the classifier
-    print(classification_report(y_test, y_pred))
+    # check metrics
+    _evaluation_metrics(actuals=y_test, prediction=y_pred, model=rf_classifier, X_val=X_test)
+
+    # predict probabilities
+    _predict_probabilities(model=rf_classifier, X_val=X_test,y_val=y_test)
 
 
 if __name__ == '__main__':
